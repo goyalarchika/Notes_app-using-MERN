d14a8ede6ba16df6ba4766ca04b7229215e11bed
goyalarchika <goyalarchika2002@gmail.com>
2024-02-27 11:03:22 +0530

    abc

---
+++ frontend/src/components/Homepage/Navbar/Navbar.jsx
@@ -0,0 +1,7 @@
+import { Box } from "@chakra-ui/react";
+
+export default function Navbar(){
+   return <Box>
+        <nav></nav>
+   </Box> 
+}
---
+++ frontend/src/routes/AllRoutes.jsx
@@ -0,0 +1,9 @@
+import { Routes, Route} from "react-router-dom";
+import Homepage from "../pages/Homepage";
+export default function AllRoutes(){
+
+    return <Routes>
+        <Route path="/" element = {<Homepage/>}></Route>
+    </Routes>
+    
+}
