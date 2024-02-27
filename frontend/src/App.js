ebcb9877479ee80b21208773d20152aa465bfa6a
goyalarchika <goyalarchika2002@gmail.com>
2024-02-27 16:57:05 +0530

    abc

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
+import { Navbar } from './components/Homepage/Navbar';
 import AllRoutes from './routes/AllRoutes';
 
 function App() {
   return (
     <div className="App">
-
+      <Navbar/>
       <AllRoutes/>
 
     </div>
--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,10 +1,13 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Heading, Image } from "@chakra-ui/react";
 import { Navbar } from "../components/Homepage/Navbar";
-
+import note from "../assests/note4.jpg"
 export default function Homepage(){
     
     return <Box>
         <Navbar/>
-        Homepage
-    </Box>
+
+        <Image w={500} position={"absolute"} src={note}/>
+        <Heading mt={16} textAlign={"start"} size={"4x1"}>Note App</Heading>
+        
+        </Box>
 }
